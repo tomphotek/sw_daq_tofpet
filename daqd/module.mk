3590c08d2b15d3bdc1dce2f4d5e9dc9e31245cf2
Tom Conneely <tom.conneely@photek.co.uk>
2017-08-18 10:22:41 +0100

    Changed daqd/module.mk to use a non-system version of Boost.
    This is to enable the use of boost::python::numpy in daqd which is
    not included in CentOS' version of boost. Locally built 1.64.0 is
    linked instead (placed in /home/tofpet/local).

--- daqd/module.mk
+++ daqd/module.mk
@@ -1,6 +1,9 @@
+BOOST_LIB_PATH := /home/tofpet/local/lib
+BOOST_INC_PATH := /home/tofpet/local/include
+
 CPPFLAGS := $(CPPFLAGS) $(shell python-config --includes)
-CXXFLAGS := -g -O2  -std=c++0x
-LDFLAGS := $(LDFLAGS) -lpthread -lrt
+CXXFLAGS := -g -O2  -std=c++0x -L$(BOOST_LIB_PATH) -I$(BOOST_INC_PATH)
+LDFLAGS := -L$(BOOST_LIB_PATH) -I$(BOOST_INC_PATH) $(LDFLAGS) -lpthread -lrt
 
 
 HEADERS := Client.hpp FrameServer.hpp UDPFrameServer.hpp DAQFrameServer.hpp DtFlyP.hpp Protocol.hpp SHM.hpp PFP_KX7.hpp
@@ -28,7 +31,7 @@ all: daqd SHM.o DSHM.so
 
 
 DSHM.so: SHM.cpp.o DSHM.cpp.o
-	$(CXX) -shared -o DSHM.so SHM.cpp.o DSHM.cpp.o $(LDFLAGS) $(shell python-config --ldflags --libs) -lboost_python-mt
+	$(CXX) -shared -o DSHM.so SHM.cpp.o DSHM.cpp.o $(LDFLAGS) $(shell python-config --ldflags --libs) -lboost_python -lboost_numpy
 
 SHM.o: SHM.cpp.o
 	cp SHM.cpp.o SHM.o

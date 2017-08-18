e71296a81cbd4b04173bc6ec189611e93277dba4
Tom Conneely <tom.conneely@photek.co.uk>
2017-08-18 10:24:33 +0100

    Added method for retrieiving a frame from shared memory as numpy array.
    This copies data from shared memory to a std::vector of structs, which
    is then cast to a numpy array using boost::python::numpy.

--- daqd/DSHM.cpp
+++ daqd/DSHM.cpp
@@ -1,12 +1,40 @@
 #include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include <boost/python/numpy.hpp>
 using namespace boost::python;
+
+#include <memory>
 
 #include "SHM.hpp"
 
 using namespace DAQd;
 
+inline void destroyArrayOwnerPtr(void *p) {
+   std::shared_ptr<PackedEventVec> *b = reinterpret_cast<std::shared_ptr<PackedEventVec>*>(p);
+   delete b;
+}
+
+inline boost::python::object makeArrayOwner(std::shared_ptr<PackedEventVec> & x) {
+    boost::python::handle<> h(PyCObject_FromVoidPtr(new std::shared_ptr<PackedEventVec>(x),
+        &destroyArrayOwnerPtr));
+    return boost::python::object(h);
+}
+
+numpy::ndarray frame2numpy(SHM& obj, int index) {
+    std::shared_ptr<PackedEventVec> frame = obj.getRawFrame(index);
+    tuple shape = make_tuple(frame->size(), 7);
+    tuple stride = make_tuple(sizeof(PackedEvent), sizeof(uint16_t));
+    numpy::dtype dt = numpy::dtype(str("u2"), false);
+
+    object own = makeArrayOwner(frame);
+    numpy::ndarray arr = numpy::from_data(frame->data(), dt, shape, stride, own);
+    return arr;
+}
+
 BOOST_PYTHON_MODULE(DSHM) 
 {
+    numpy::initialize();
+
 	class_<SHM>("SHM", init<std::string>())
 		.def("getSizeInBytes", &SHM::getSizeInBytes)
 		.def("getSizeInFrames", &SHM::getSizeInFrames)
@@ -25,5 +53,7 @@ BOOST_PYTHON_MODULE(DSHM)
 		.def("getTACID", &SHM::getTACID)
 		.def("getTACIdleTime", &SHM::getTACIdleTime)
 		.def("getChannelIdleTime", &SHM::getChannelIdleTime)
+		//.def("getRawFrame", &SHM::getRawFrame)
+		.def("getNumpyFrame", &frame2numpy)
 	;
 }
--- daqd/SHM.hpp
+++ daqd/SHM.hpp
@@ -4,6 +4,8 @@
 #include "Protocol.hpp"
 #include <sys/types.h>
 #include <string>
+#include <vector>
+#include <memory>
 
 namespace DAQd {
 	
@@ -21,7 +23,23 @@ struct DataFrame {
 #ifdef __ENDOTOFPET__
 		int8_t feType[MaxDataFrameSize];
 #endif
-};	
+};
+
+typedef struct PackedEvent {
+    uint16_t asic_id;
+    uint16_t chan_id;
+    uint16_t tac_id;
+    uint16_t tcoarse;
+    uint16_t tfine;
+    uint16_t ecoarse;
+    uint16_t efine;
+
+    bool operator== (const PackedEvent& rhs) {
+        return this->asic_id == rhs.asic_id;
+    }
+} PackedEvent_t;
+
+typedef std::vector<PackedEvent_t> PackedEventVec;
 
 class SHM {
 public:
@@ -193,11 +211,43 @@ public:
 		return 0;
 #endif
 	};
+
+	std::shared_ptr<PackedEventVec>  getRawFrame(int index) {
+	    int nevents = getNEvents(index);
+	    int evCount = 0;
+	    DataFrame *dataFrame = &shm[index];
+	    std::shared_ptr<PackedEventVec> procFrame(new PackedEventVec());
+
+	    for (int i = 0; i < nevents; ++i) {
+	        //if (dataFrame->feType[i+2] == 0) {
+            uint64_t eventWord = dataFrame->data[i+2];
+            PackedEvent_t p;
+
+            p.tcoarse = (eventWord >> 38) & 0x3FF;
+            p.ecoarse = (eventWord >> 18) & 0x3FF;
+            p.tfine = (eventWord >> 28) & 0x3FF;
+            p.efine = (eventWord >> 8) & 0x3FF;
+
+            p.tac_id = eventWord & 0x3;
+            p.chan_id = (eventWord >> 2) & 0x3F;
+
+            uint64_t idWord = eventWord >> 48;
+            uint64_t asicID = idWord & 0x3F;
+            uint64_t slaveID = (idWord >> 6) & 0x1F;
+            uint64_t portID = (idWord >> 11) & 0x1F;
+            p.asic_id = (slaveID & 0x1) * 16*16 + (portID & 0xF) * 16 + (asicID & 0xF);
+
+            procFrame->push_back(p);
+	        //}
+	    }
+
+	    return procFrame;
+	}
   
 
 private:
 	unsigned decodeSticCoarse(unsigned coarse, unsigned long long frameID);
-	
+
 	int shmfd;
 	DataFrame *shm;
 	off_t shmSize;
@@ -205,5 +255,33 @@ private:
 	int16_t m_lut[ 1 << 15 ];
 };
 
+//}
+
+class SHM_TEST {
+    public:
+        SHM_TEST() {};
+
+        std::shared_ptr<PackedEventVec> getRawFrame() {
+            int nevents = 5;
+            std::shared_ptr<PackedEventVec> procFrame(new PackedEventVec());
+
+            for (int i = 0; i < nevents; ++i) {
+                PackedEvent_t p;
+
+                p.tcoarse = i + 1;
+                p.ecoarse = i;
+                p.tfine = i;
+                p.efine = i;
+
+                p.tac_id = i;
+                p.chan_id = i;
+                p.asic_id = i;
+
+                procFrame->push_back(p);
+            }
+
+            return procFrame;
+        }
+};
 }
 #endif
